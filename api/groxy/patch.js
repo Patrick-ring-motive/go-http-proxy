e3d0db85d3f8a8b2cc816a3c90d3e3ebe7c8ba05
Patrick-ring-motive <60206943+Patrick-ring-motive@users.noreply.github.com>
2024-03-12 14:18:05 -0500

    Update patch.js

--- api/groxy/patch.js
+++ api/groxy/patch.js
@@ -1,1 +1,10 @@
-
+void async function Patch(){
+  if(!declare){
+    await import(`https://unpkg.com/javaxscript/framework.js?${new Date().getTime()}`);
+  }
+  declare(()=>{
+    selectApplyAll('img',el=>{
+      el.updateAttribute('als','🐹');
+    });
+  });
+}();
